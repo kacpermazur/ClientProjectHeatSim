0d725cc96883da6389e75a671f1fb3140b3e5dea
Kacper Mazur <kacper.mazur888@gmail.com>
2019-03-25 21:43:30 +0000

    ImportedSlider

--- heat-sim/src/App.js
+++ heat-sim/src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-import Slider from "./componets/Slider";
+import Slider from "react-rangeslider";
+import "react-rangeslider/lib/index.css";
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      volume: 0
+    };
+  }
+
+  handleOnChange = value => {
+    this.setState({
+      volume: value
+    });
+
+    console.log(value);
+  };
   render() {
+    let { volume } = this.state;
     return (
       <React.Fragment>
         <div>Temp</div>
-        <Slider />
+        <div>Temp</div>
+        <div>Temp</div>
+        <div>Temp</div>
+
+        <Slider
+          value={volume}
+          orientation="vertical"
+          onChange={this.handleOnChange}
+        />
       </React.Fragment>
     );
   }
