e991b9d03527aa29b7024ab57541c5a30cb61740
Kacper Mazur <kacper.mazur888@gmail.com>
2019-03-25 22:44:45 +0000

    Added Sliders that change specific Variables Values

--- heat-sim/src/App.js
+++ heat-sim/src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from "react";
-import Slider from "react-rangeslider";
-import "react-rangeslider/lib/index.css";
+
+import InteractHolder from "./component/Interacts/InteractHolder";
 
 class App extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      volume: 0
-    };
-  }
-
-  handleOnChange = value => {
-    this.setState({
-      volume: value
-    });
-
-    console.log(value);
-  };
   render() {
-    let { volume } = this.state;
     return (
       <React.Fragment>
-        <div>Temp</div>
-        <div>Temp</div>
-        <div>Temp</div>
-        <div>Temp</div>
-
-        <Slider
-          value={volume}
-          orientation="vertical"
-          onChange={this.handleOnChange}
-        />
+        <center>
+          <div>temp</div>
+          <div>temp</div>
+          <InteractHolder />
+        </center>
       </React.Fragment>
     );
   }
