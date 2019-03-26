891b297cac0bf29d6917880a4bfb65f05501a76c
kacper <kacper.mazur888@gmail.com>
2019-03-26 14:56:45 +0000

    valueDisplay

--- heat-sim/src/App.js
+++ heat-sim/src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 
 import InteractHolder from "./component/Interacts/InteractHolder";
 import RenderView from "./component/RenderView/RenderView";
+import ValueDisplay from "./component/ValueDisplay/ValueDisplay";
 
 class App extends Component {
   render() {
+    let props = {
+      TemptureTextAir: "test1",
+      DayText: "test2",
+      TemptureTextWall: "test3"
+    };
+
     return (
       <React.Fragment>
         <center>
           <RenderView />
-          <div>temp</div>
+          <ValueDisplay TemptureTextAir={props} />
           <InteractHolder />
         </center>
       </React.Fragment>
